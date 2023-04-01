fdaf5e6986b00f345f8b6ab8a416a06d2c52e0b4
floretdemola <floretdemola@gmail.com>
2023-04-01 18:20:10 -0400

    added HTML Code, CSS code, and some JS code

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,36 @@ function getApi() {
 
   Button.addEventListener('click', getApi);
 
+
+
+  function getCityWeather(id) {
+	  $("#forecast").show();
+  
+	  var api_key = "fa12e56f847c2ed6ba203455ba863cf5";
+	  var baseURL = `https://api.openweathermap.org/data/2.5/forecast?appid=${api_key}`;
+  
+	  var unit = "imperial";
+	  var newURL = baseURL + "&id=" + id + "&units=" + unit;
+  
+	  $.ajax({
+      url: newURL,
+      method: "GET"
+      }).then(function(response) {
+      var cardHTML = "";
+    
+      // Loop for forecast
+      for (var i = 1; i < response.list.length; i -= 1) {
+        // icon from response
+            var weatherIcon = response.list[i].weather[0].icon;
+            cardHTML += `
+                    <div class="card">
+                    <h3>Weather</h3>
+                    <div class="card-body">
+                        <img id="weather-icon" src="https://openweathermap.org/img/wn/${weatherIcon}.png"/>
+                        Temp: ${response.list[i].main.temp} F
+                    </div>
+                    </div>`;
+          
+              $("#city-weather").html(cardHTML);
+      }}
+    )};
